8a85a833b9054095bfa0678324784125ffd87cee
Kevin Babauta <kjr.babauta@gmail.com>
2022-09-20 03:16:27 -0700

    fixed links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/kevinbabauta/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/kbabauta" target="_blank"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/kevinbabauta/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/kbabauta" target="_blank" rel="noreferrer"><FaGithub/></a>
     </div>
   )
 }
